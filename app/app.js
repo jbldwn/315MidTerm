1dd4fb5967179c73ec786745c69e975e6d37b6d4
jbldwn <58793978+jbldwn@users.noreply.github.com>
2023-10-09 11:12:25 -0400

    catchup

--- app/app.js
+++ app/app.js
@@ -11,11 +11,38 @@ function route() {
     getData(hash);
 }
 
+function displayNav() {
+    let links = document.getElementsByClassName("links");
+
+    // console.log(links);
+    // links.css("display",) = 'flex';
+    console.log("clicked");
+}
+
 function initListeners() {
     $(window).on("hashchange", route);
+    $(".mobileNav").on("click", displayNav);
 }
+
+// function makeInvisible() {
+//     let mobileNav = document.getElementsByClassName("mobileNav");
+//     let links = document.getElementsByClassName("links");
+//     let currentInvisible = document.getElementById("invisible");
+
+//     console.log(currentInvisible);
+
+//     currentInvisible
+
+//     /* if mobile nav is visible, links is not */
+//     /* if links is visible mobile nav is not */
+//     // mobileNav.idName = mobileNav.idName === "invisible" ? "" : "invisible";
+
+//     // console.log(mobileNav);
+//     // console.log(mobileNav.className);
+// }
 
 $(document).ready(function () {
     route();
+    // makeInvisible();
     initListeners();
 });
